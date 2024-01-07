cbfa9654dd650e760c5528ab029f53a6b75d146f
nur farazi <nur369188@gmail.com>
2024-01-07 21:33:05 +0600

    Add Quiz model to the project

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Quiz(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
